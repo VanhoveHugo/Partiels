42ad442db9b59a1ba86673154a7a132c799bec07
VanhoveHugo <74211478+VanhoveHugo@users.noreply.github.com>
2020-12-14 11:08:14 +0100

    PoudreBlanche 14/12

--- PoudreBlanche/preset/languages/en.php
+++ PoudreBlanche/preset/languages/en.php
@@ -3,6 +3,16 @@
         "v" => "Fr",
         "logo_baseline" => "Restoration",
 
+		"main_h1" => "We invite you to join us on Monday January 4 for the opening of our restaurant!",
+		"main_p" => "Your cooked meals served warmly, every week. Give yourself more pleasure in life, like in the kitchen.",
+		"main_button" => "Discover",
+
+
+		"carte_button" => "See more",
+
+		"footer_l" => "Delyrevoo",
+		"footer_m" => "Join us",
+		"footer_p" => "Site map",
 
 		"menu" => "Home",
 		"about" => "About",
--- PoudreBlanche/preset/languages/fr.php
+++ PoudreBlanche/preset/languages/fr.php
@@ -1,8 +1,19 @@
 <?php
 	$lang = array(
         "v" => "En",
-        "logo_baseline" => "Restauration",
+		"logo_baseline" => "Restauration",
+		
+		"main_h1" => "Nous vous invitons à nous rejoindre le lundi 4 janvier pour l'ouverture de notre restaurant!",
+		"main_p" => "Vos repas cuisinés servis chaleureusement, chaque semaine. Offrez-vous plus de plaisir dans la vie, comme dans la cuisine.",
+		"main_button" => "Découvrir",
 
+
+		"carte_button" => "Voir plus",
+
+
+		"footer_l" => "Livraison",
+		"footer_m" => "Nous rejoindre",
+		"footer_p" => "Plan du site",
 
 		"menu" => "Accueil",
 		"about" => "A propos",
